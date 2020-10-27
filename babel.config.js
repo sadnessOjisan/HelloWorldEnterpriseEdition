271342dfd286ff91610379515f36d62912cd03b5
sadness_ojisan <sadness.ojisan@gmail.com>
2020-10-28 01:47:22 +0900

    chore: tsx

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   // TODO: preset を全部pluginにバラす
   plugins: [
-    "@babel/plugin-transform-typescript",
+    ["@babel/plugin-transform-typescript", { isTSX: true }],
+    "@babel/plugin-syntax-jsx",
     "@babel/plugin-transform-react-jsx",
+    "@babel/plugin-transform-react-display-name",
+    "@babel/plugin-transform-react-jsx-self",
+    "@babel/plugin-transform-react-jsx-source",
   ],
-  presets: ["@babel/env", "@babel/preset-typescript"],
+  presets: ["@babel/env"],
 };
