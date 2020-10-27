edcf4b629afb8855647b7273954400c80d8d1025
sadness_ojisan <sadness.ojisan@gmail.com>
2020-10-28 02:13:25 +0900

    chore: del unused plugin

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,7 @@ module.exports = {
   // TODO: preset を全部pluginにバラす
   plugins: [
     ["@babel/plugin-transform-typescript", { isTSX: true }],
-    "@babel/plugin-syntax-jsx",
     "@babel/plugin-transform-react-jsx",
-    "@babel/plugin-transform-react-display-name",
-    "@babel/plugin-transform-react-jsx-self",
-    "@babel/plugin-transform-react-jsx-source",
   ],
   presets: ["@babel/env"],
 };
